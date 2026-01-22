c43022ae94977552ec86dca0c7257520950da0f9
Laia Querol Alturo <laiaquerola27@gmail.com>
2026-01-22 02:15:56 +0100

    lang fix

---
+++ public/assets/js/i18n.js
@@ -0,0 +1,59 @@
+const translations = {
+  en: {
+    greeting: "Hello!",
+    name: "I'm ",
+    presentation: `Welcome to my website!
+                   This is a space where I share what I do, what I’m learning, and what I’m excited to build next. It serves as a portfolio of my work, but it is also a reflection of how I think and create.`,
+    contact_button: "Contact me",
+    home: "Home",
+    education: "Education",
+    experience: "Experience",
+    projects: "Projects",
+    cv: "CV",
+    blog: "Blog",
+    matcad: "<strong>Bachelor's</strong> in Computational Mathematics and Data Analytics",
+    batxillerat: "Scientific-Technological Baccalaureate",
+    cvc: "Computer Vision Center",
+    cvc_internship: "Machine Learning Intern",
+    cvc_time2: "Sep 2024 - Feb 2025",
+    cvc_time2_text: "Extracurricular internship in the Medical Imaging team. Following up on previous internship.",
+    cvc_time1: "Feb 2024 - Jun 2024",
+    cvc_time1_text: "Obtained a grant to work in the Medical Imaging team. Working on tasks in Machine Learning.",
+    rosa_sensat: "Rosa Sensat Scolarship",
+    portfolio: "Portfolio",
+    erasmus: "<strong>Erasmus+</strong> Program",
+    scroll_down: "Scroll down",
+    footer: "2026 Laia Querol Alturo - All rights reserved."
+  },
+
+  ca: {
+    greeting: "Hola!",
+    name: "Soc la ",
+    presentation: `Benvinguts al meu web! 
+                   Aquest és un espai on comparteixo el que faig, el que estic aprenent i el que em fa il·lusió construir a continuació. Serveix com a portfoli de la meva feina, però també és un reflex de com penso i creo.`,
+    contact_button: "Contacta’m",
+    home: "Inici",
+    education: "Educació",
+    experience: "Experiència",
+    projects: "Projectes",
+    cv: "CV",
+    blog: "Blog",
+    matcad: "<strong>Grau</strong> en Matemàtica Computacional i Analítica de Dades",
+    batxillerat: "Batxillerat Científic-Tecnològic",
+    cvc: "Centre de Visió per Computador",
+    cvc_internship: "Becària en Aprenentatge Computacional",
+    cvc_time2: "Set 2024 - Feb 2025",
+    cvc_time2_text: "Pràctiques extracurriculars en l'equip d'Imatge Mèdica. Continuació de les pràctiques anteriors.",
+    cvc_time1: "Feb 2024 - Jun 2024",
+    cvc_time1_text: "Obtinguda una beca per treballar a l'equip d'Imatge Mèdica. Treballant en tasques d'Aprenentatge Computacional.",
+    rosa_sensat: "Beca Rosa Sensat",
+    portfolio: "Portfoli",
+    erasmus: "Programa <strong>Erasmus+</strong>",
+    scroll_down: "Desplaça't cap avall",
+    footer: "2026 Laia Querol Alturo - Tots els drets reservats."
+  }
+};
+
+if (typeof window !== "undefined") {
+  window.translations = translations;
+}

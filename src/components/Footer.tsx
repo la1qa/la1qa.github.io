2ebacdced23698fd34f3c961458ab61126dda808
Laia Querol Alturo <laiaquerola27@gmail.com>
2026-01-22 02:54:10 +0100

    footer css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './styles/Footer.module.css';
 
 const Footer: React.FC = () => {
   return (
-    <footer id="contact" className="footer section">
-      <div className="footer__container bd-grid">
-        <h3 className="footer__copy" data-i18n="footer"></h3>
+    <footer id="contact" className={styles.footer}>
+      <div className={styles.footer__container}>
+        <h3 className={styles.footer__copy} data-i18n="footer"></h3>
       </div>
     </footer>
   );
